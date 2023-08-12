dba24ae2340fdd222c80cb4d118e6845819a2cb6
Andhika Prasetya <andhikapr081@gmail.com>
2023-08-12 19:01:44 +0700

    Add search functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from "react";
 
-import { Wrap, WrapItem, Heading, Text, Center } from "@chakra-ui/react";
+import {
+  Wrap,
+  WrapItem,
+  Heading,
+  Text,
+  Center,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
+import { Search2Icon } from "@chakra-ui/icons";
 
 // utils
 import { getVideos } from "../utils";
@@ -8,6 +18,7 @@ import { Card } from "../component";
 
 const Home = () => {
   const [videos, setVideos] = useState([]);
+  const [search, setSearch] = useState("");
 
   const getData = async () => {
     const response = await getVideos();
@@ -20,10 +31,25 @@ const Home = () => {
     getData();
   }, []);
 
+  const filteredVideos = videos.filter((video) => {
+    return video.title.toLowerCase().includes(search.toLowerCase());
+  });
+
   return (
     <>
+      <InputGroup>
+        <InputLeftElement>
+          <Search2Icon />
+        </InputLeftElement>
+        <Input
+          variant="flushed"
+          placeholder="Search Product"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </InputGroup>
+
       <Wrap spacing="20px" justify={"center"}>
-        {videos.map((video, index) => {
+        {filteredVideos.map((video, index) => {
           return (
             <WrapItem key={video._id}>
               <Card video={video} type="video" />

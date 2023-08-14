84d4f015e5c9a8d60bed674c6e639e0208f3c81f
Andhika-GIT <andhikapr081@gmail.com>
2023-08-15 03:33:18 +0700

    fix error when add comment on videoDetail

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,39 +1,23 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import {
-  Box,
-  Card,
-  CardBody,
-  CardHeader,
-  Center,
-  Divider,
-  Heading,
-  Stack,
-  StackDivider,
-  Text,
-} from "@chakra-ui/react";
+import { Box, Card, CardBody, CardHeader, Center, Divider, Heading, Stack, StackDivider, Text } from '@chakra-ui/react';
 
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
 // utils
-import {
-  createComment,
-  getComment,
-  getProduct,
-  getVideoDetail,
-} from "../utils";
+import { createComment, getComment, getProduct, getVideoDetail } from '../utils';
 
 // SWIPER
-import "swiper/css";
-import "swiper/css/navigation";
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 // COMPONENTS
-import { AddComment, Comment, Error, Loading, ProductList } from "../component";
+import { AddComment, Comment, Error, Loading, ProductList } from '../component';
 
-import ReactPlayer from "react-player/youtube";
+import ReactPlayer from 'react-player/youtube';
 
 const VideoDetail = () => {
-  const [video, setVideo] = useState("");
+  const [video, setVideo] = useState('');
   const [products, setProducts] = useState([]);
   const [comments, setComments] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -58,6 +42,12 @@ const VideoDetail = () => {
     setIsLoading(false);
   };
 
+  // refresh comment
+  const fetchCommentsData = async () => {
+    const response = await getComment(videoId);
+    setComments(response.data.data);
+  };
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -75,9 +65,7 @@ const VideoDetail = () => {
   };
 
   if (isError) {
-    return (
-      <Error message="Something wrong, to go back to home page and try again" />
-    );
+    return <Error message="Something wrong, to go back to home page and try again" />;
   }
 
   if (isLoading) {
@@ -86,25 +74,16 @@ const VideoDetail = () => {
 
   return (
     <Box mb={20}>
-      <Center d="flex" flexDirection={"column"} gap={50}>
-        <Card w={"full"}>
+      <Center d="flex" flexDirection={'column'} gap={50}>
+        <Card w={'full'}>
           <CardBody>
-            <ReactPlayer
-              url={video.videoURL}
-              width="full"
-              controls="true"
-              style={{ boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px" }}
-            />
+            <ReactPlayer url={video.videoURL} width="full" controls="true" style={{ boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px' }} />
             <Stack mt="6" spacing="3" mb="3">
               <Heading size="md">{video.title}</Heading>
               <Text>{video.description}</Text>
             </Stack>
             <Divider mb="10" />
-            {products.length > 0 ? (
-              <ProductList products={products} />
-            ) : (
-              <Error message="This shop has no product" />
-            )}
+            {products.length > 0 ? <ProductList products={products} /> : <Error message="This shop has no product" />}
           </CardBody>
         </Card>
         <AddComment onAddComment={submitComment} loading={isCommentLoading} />
